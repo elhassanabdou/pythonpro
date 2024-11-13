46f33db43cb171fa2a4186cd853bee075fda9c92
elhassan abdou <elhassan.abdou@gmail.com>
2024-11-13 16:48:56 +0100

    fix date format. Adding return car function

--- project.py
+++ project.py
@@ -4,9 +4,8 @@ def show_menu():
     print('\nTo-Do List Manager')
     print('1. Show cars')
     print('2. Rent cars')
-    print('3. Remove a task')
+    print('3. Return vehicle')
     print('4. Save tasks to a file')
-    print('5. Load tasks from a file')
     print('6. Exit')
     choice = input('Enter your choise (1-6): ')
     return choice
@@ -42,6 +41,32 @@ def add_customer(customers):
     #write to customers file
     write_file('customers.txt',customers)
 
+def calculate_cost(rented_date, cost):
+    delta = datetime.datetime.now() - datetime.datetime.strptime(rented_date,"%d/%m/%Y %H:%M")
+    days = int(delta.days)
+    if(days < 1):
+        days =1
+    return days * cost
+
+def return_vehicle():
+    regNumber = input("Give the register number of the car you want to rent: ")
+    available_vehicles = read_file('vehicles.txt')
+    rentedCars = read_file('rentedVehicles.txt')
+
+    if regNumber in available_vehicles:
+        if regNumber in rentedCars:
+            cost = float(available_vehicles[regNumber][1])
+            rented_date = rentedCars[regNumber][1]
+            totalCost = calculate_cost(rented_date,cost)
+            print(f"Total cost '{totalCost}'")
+            #remove from rentedCars
+            del rentedCars[regNumber]
+            write_file('rentedVehicles.txt',rentedCars)
+        else:
+            print("car was not rented")
+    else:
+        print("Car does not exist")
+
 
 def rent_vehicle():
     available_vehicles = read_file('vehicles.txt')
@@ -56,7 +81,7 @@ def rent_vehicle():
         else:
             add_customer(customers)
         #move the vehicle to rented file
-        currentdate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        currentdate = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
         rentedCars[regNumber]=[customerBD,currentdate]
         write_file('rentedVehicles.txt',rentedCars)   
     elif regNumber in rentedCars:
@@ -71,6 +96,9 @@ def main():
             show_vechile()
         elif choice == '2':
             rent_vehicle()
+        elif choice == '3':
+            return_vehicle()
+        
         elif choice == '6':
             exit()
 
