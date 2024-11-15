ca2871915a75dfcd7521d06f9b8e412c6207f672
ManWithNothingToLose <124495934+ManWithNothingToLose@users.noreply.github.com>
2024-11-15 18:02:14 +0200

    project.py
    Count_the_money added. Menu accurated. Added age check on Rent vehicle but it is not working effective. Still errors are on rent car and return car (program crashes).

--- project.py
+++ project.py
@@ -1,13 +1,13 @@
 import datetime
 
 def show_menu():
-    print('\nTo-Do List Manager')
-    print('1. Show cars')
-    print('2. Rent cars')
-    print('3. Return vehicle')
-    print('4. Save tasks to a file')
-    print('6. Exit')
-    choice = input('Enter your choise (1-6): ')
+    print('\nYou may select one of the following:')
+    print('1. List available cars')
+    print('2. Rent a car')
+    print('3. Return a car')
+    print('4. Count the money')
+    print('0. Exit')
+    choice = input('What is your selection? ')
     return choice
 
 def read_file(file_path, delimiter=','):
@@ -27,7 +27,7 @@ def write_file(file_path, data, delimiter=","):
             line = delimiter.join([key]+values)
             file.write(line + '\n')
 
-def show_vechile():
+def show_vehicle():
     available_vehicles = read_file('vehicles.txt')
     for key, values in available_vehicles.items():
         properties = ",".join(values)
@@ -63,7 +63,7 @@ def return_vehicle():
             del rentedCars[regNumber]
             write_file('rentedVehicles.txt',rentedCars)
         else:
-            print("car was not rented")
+            print("Car was not rented")
     else:
         print("Car does not exist")
 
@@ -73,11 +73,19 @@ def rent_vehicle():
     customers = read_file('customers.txt')
     rentedCars = read_file('rentedVehicles.txt')
     regNumber = input("Give the register number of the car you want to rent: ")
+    
     if regNumber in available_vehicles:
         customerBD = input("Please enter your birthday in form DD/MM/YYYY: ")
         if customerBD in customers:
             customer = customers[customerBD]
-            print(f"Welcome f'{customer[0]}' f'{customer[1]}'")
+            print(f"Hello f'{customer[0]}' f'{customer[1]}'")
+
+
+        elif customerBD > ('01/01/2006'):
+            print("You are too young to rent a car")
+        elif customerBD > ('01/01/1950'):
+            print("You are too old for rent a car")
+            
         else:
             add_customer(customers)
         #move the vehicle to rented file
@@ -88,19 +96,42 @@ def rent_vehicle():
         print("Car is already rented")
     else:
         print("Car does not exist")
+def count_the_money():
+
+    money = 0.00
+
+    with open('transActions.txt', 'r') as i:
+        for line in i:
+            total_price = float(line.strip().split(",")[-1])
+            total_earning =+ total_price
+
+        print(f"Total Earnings: ${total_earning:2f}")
+    
 
 def main():
     while True:
         choice = show_menu()
         if choice == '1':
-            show_vechile()
+            show_vehicle()
         elif choice == '2':
             rent_vehicle()
         elif choice == '3':
             return_vehicle()
-        
-        elif choice == '6':
-            exit()
+        elif choice == '4':
+            count_the_money()
+            
+        elif choice == '0':
+            print("Bye!")
+            break
+            
+        else:
+        print("invalid selection. Try again")
+
+
+if __name__ == "__main__":
+    main()
+
+
 
 
 if __name__ == "__main__":
